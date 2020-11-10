7ad3d21c07df0e243ac56c04730ebe5d2e45d9f3
juanni0210 <janeni1020@gmail.com>
2020-11-10 00:36:25 -0500

    createa a simple calculator on website using JavaScript, HTML and CSS

---
+++ js/script.js
@@ -0,0 +1,81 @@
+let btns;
+let displayText;
+let num1;
+let num2;
+let operator;
+let result;
+let isSecondNum;
+
+window.onload = function () {
+    btns = document.querySelectorAll(".btn");
+    displayText = document.querySelector(".form-control");
+
+    for (let btn of btns) {
+        btn.addEventListener("click", handleCalculate);
+    }
+
+    resetData();
+}
+
+function calculate() {
+    let tempNum1 = parseFloat(num1);
+    let tempNum2 = parseFloat(num2);
+
+    switch (operator) {
+        case '+':
+            result = tempNum1 + tempNum2;
+            break;
+        case '-':
+            result = tempNum1 - tempNum2;
+            break;
+        case '*':
+            result = tempNum1 * tempNum2;
+            break;
+        case '/':
+            result = tempNum1 / tempNum2;
+            break;
+    }
+    displayText.value = result.toString();
+    resetData();
+    return false;
+}
+
+
+function handleCalculate(e) {
+
+    let eventTargetObject = e.target;
+    let btnLabel = eventTargetObject.value;
+    if (btnLabel === '=') {
+        return;
+    }
+    if (btnLabel === 'C') {
+        resetData();
+        clearUI();
+        return;
+    }
+
+    let isDigit = parseInt(btnLabel);
+    if (!isNaN(isDigit) || btnLabel === '.') {
+        if (!isSecondNum) {
+            num1 += btnLabel;
+        } else {
+            num2 += btnLabel;
+        }
+    } else {
+        operator = btnLabel;
+        isSecondNum = true;
+    }
+
+    displayText.value = displayText.value + btnLabel;
+}
+
+function resetData() {
+    num1 = '';
+    num2 = '';
+    operator = '';
+    isSecondNum = false;
+}
+
+function clearUI() {
+    displayText.value = '';
+}
