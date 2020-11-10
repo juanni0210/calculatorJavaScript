0ca88717b44fa0b574cbf3509572b8117cf3dd03
ziv <ziv.hao.shek@gmail.com>
2020-11-10 18:49:11 -0500

    Refactor the app to use the state machine pattern.

--- js/script.js
+++ js/script.js
@@ -1,93 +1,152 @@
-let btns;
-let displayText;
-let num1;
-let num2;
-let operator;
-let result;
-let isSecondNum;
-let newCalculation;
+const states = {
+    left: 'left',
+    right: 'right',
+    equal: 'equal'
+};
+
+const keyTypes = {
+    digit: 'digit', // including dot
+    op: 'op',
+    equal: 'equal',
+    clear: 'clear'
+};
+
+const updateDisplayAction = {
+    set: 'set',
+    append: 'append'
+};
+
+let calculator = {
+    state: states.left,
+    left: '',
+    right: '',
+    op: '',
+    display: null,
+    initialize: function (displayText) { this.display = displayText; },
+    onKey: function (keyType, keyString) {
+        if (keyType === keyTypes.clear) {
+            this.reset();
+            return;
+        }
+
+        switch (this.state) {
+            case states.left:
+                this.onLeft(keyType, keyString);
+                break;
+            case states.right:
+                this.onRight(keyType, keyString);
+                break;
+            case states.equal:
+                this.onEqual(keyType, keyString);
+                break;
+        }
+    },
+    onLeft: function (keyType, keyString) {
+        switch (keyType) {
+            case keyTypes.digit:
+                this.left += keyString;
+                break;
+            case keyTypes.op:
+                this.op = keyString;
+                this.state = states.right;
+                break;
+            default:
+                return;
+        }
+        this.setDisplay(keyString, updateDisplayAction.append);
+    },
+    onRight: function (keyType, keyString) {
+        switch (keyType) {
+            case keyTypes.digit:
+                this.right += keyString;
+                this.setDisplay(keyString, updateDisplayAction.append);
+                break;
+            case keyTypes.op:
+                this.left = this.calculate();
+                this.setDisplay(this.left, updateDisplayAction.set);
+                this.op = keyString;
+                this.right = '';
+                this.setDisplay(keyString, updateDisplayAction.append);
+                break;
+            case keyTypes.equal:
+                this.reset(this.calculate().toString());
+                this.state = states.equal;
+                break;
+        }
+    },
+    onEqual: function (keyType, keyString) {
+        switch (keyType) {
+            case keyTypes.digit:
+                this.reset(keyString);
+                break;
+            case keyTypes.op:
+                this.left = this.display.value;
+                this.op = keyString;
+                this.setDisplay(keyString, updateDisplayAction.append);
+                this.state = states.right;
+                break;
+        }
+    },
+    calculate: function () {
+        let result;
+        let left = parseFloat(this.left);
+        let right = parseFloat(this.right);
+        switch (this.op) {
+            case '+':
+                result = left + right;
+                break;
+            case '-':
+                result = left - right;
+                break;
+            case '*':
+                result = left * right;
+                break;
+            case '/':
+                result = left / right;
+                break;
+        }
+        return result;
+    },
+    setDisplay: function (newContent, action) {
+        switch (action) {
+            case updateDisplayAction.set:
+                this.display.value = newContent;
+                break;
+            case updateDisplayAction.append:
+                this.display.value += newContent;
+                break;
+        }
+    },
+    reset: function (withLeft = '') {
+        this.left = withLeft;
+        this.right = '';
+        this.op = '';
+        this.state = states.left;
+        this.setDisplay(withLeft, updateDisplayAction.set);
+    }
+};
 
 window.onload = function () {
     btns = document.querySelectorAll(".btn");
     displayText = document.querySelector(".form-control");
+    calculator.initialize(displayText);
 
     for (let btn of btns) {
-        btn.addEventListener("click", handleCalculate);
+        let label = btn.value;
+        let type = getKeyType(label);
+        btn.addEventListener("click", (e) => {
+            calculator.onKey(type, label);
+        });
     }
-
-    resetData();
 }
 
-function calculate() {
-    let tempNum1 = parseFloat(num1);
-    let tempNum2 = parseFloat(num2);
-    if(operator.length > 1) {
-        displayText.value = "Error";
-        resetData();
-        return false;
-    }
-
-    switch (operator) {
-        case '+':
-            result = tempNum1 + tempNum2;
-            break;
-        case '-':
-            result = tempNum1 - tempNum2;
-            break;
-        case '*':
-            result = tempNum1 * tempNum2;
-            break;
-        case '/':
-            result = tempNum1 / tempNum2;
-            break;
-    }
-    displayText.value = result.toString();
-    resetData();
-    return false;
-}
-
-
-function handleCalculate(e) {
-    
-    if(newCalculation) {
-        clearUI();
-        newCalculation = false;
-    }
-
-    let eventTargetObject = e.target;
-    let btnLabel = eventTargetObject.value;
-    if (btnLabel === '=') {
-        return;
-    }
-    if (btnLabel === 'C') {
-        resetData();
-        clearUI();
-        return;
-    }
-
-    let isDigit = parseInt(btnLabel);
-    if (!isNaN(isDigit) || btnLabel === '.') {
-        if (!isSecondNum) {
-            num1 += btnLabel;
-        } else {
-            num2 += btnLabel;
-        }
-    } else {
-        operator += btnLabel;
-        isSecondNum = true;
-    }
-
-    displayText.value = displayText.value + btnLabel;
-}
-
-function resetData() {
-    num1 = '';
-    num2 = '';
-    operator = '';
-    isSecondNum = false;
-    newCalculation = true;
-}
-
-function clearUI() {
-    displayText.value = '';
+function getKeyType(label) {
+    if (!isNaN(parseInt(label)) || label === '.')
+        return keyTypes.digit;
+    if (label === '+' || label === '-' || label === '*' || label === '/')
+        return keyTypes.op;
+    if (label === 'C')
+        return keyTypes.clear;
+    if (label === '=')
+        return keyTypes.equal;
 }
